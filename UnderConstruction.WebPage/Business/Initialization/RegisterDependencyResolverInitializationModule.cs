257223541383054564cef9b82dc81a716407beae
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-21 17:39:03 +0100

    Added dependency resolver via structuremap

---
+++ UnderConstruction.WebPage/Business/Initialization/RegisterDependencyResolverInitializationModule.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using UnderConstruction.WebPage.Business.DependencyResolvers;
+
+namespace UnderConstruction.WebPage.Business.Initialization
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class RegisterDependencyResolverInitializationModule : IConfigurableModule
+    {
+        public void Initialize(InitializationEngine context)
+        { }
+
+        public void Uninitialize(InitializationEngine context)
+        { }
+
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(context.StructureMap()));
+
+            context.ConfigurationComplete += (o, e) =>
+            {
+                //Register custom implementations that should be used in favour of the default implementations
+            };
+        }
+    }
+}
