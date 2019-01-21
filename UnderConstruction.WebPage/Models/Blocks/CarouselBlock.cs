1caaf3b212c6abb02981ded5284a155f85eaf983
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-21 23:48:30 +0100

    Added carousel block types

---
+++ UnderConstruction.WebPage/Models/Blocks/CarouselBlock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAnnotations;
+
+namespace UnderConstruction.WebPage.Models.Blocks
+{
+    [ContentType(DisplayName = "CarouselBlock", 
+        GUID = "778225b0-fe7c-405c-9a78-9700a3184cc5", 
+        Description = "")]
+    public class CarouselBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(Name = "Carousel items")]
+        [AllowedTypes(typeof(CarouselItemBlock))]
+        public virtual IList<ContentReference> Items { get; set; }
+    }
+}
---
+++ UnderConstruction.WebPage/Models/Blocks/CarouselItemBlock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using EPiServer.Web;
+
+namespace UnderConstruction.WebPage.Models.Blocks
+{
+    [ContentType(DisplayName = "CarouselItemBlock",
+        GUID = "a2d34a78-31c9-4584-9c54-936803ca6ec5",
+        Description = "")]
+    public class CarouselItemBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(Name = "Small header",
+            Order = 10)]
+        public virtual string SmallHeader { get; set; }
+
+        [Required]
+        [Display(Name = "Background image",
+            Order = 15)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference BackgroundImage { get; set; }
+
+        [Required]
+        [Display(Name = "Big header",
+            Order = 20)]
+        public virtual string BigHeader1 { get; set; }
+
+        [Display(Name = "Big header 2",
+            Order = 30)]
+        public virtual string BigHeader2 { get; set; }
+
+        [Required]
+        [Display(Name = "Paragraph",
+            Order = 40)]
+        public virtual XhtmlString Paragraph { get; set; }
+
+        [Display(Name = "Links",
+            Order = 50)]
+        public virtual LinkItemCollection Links { get; set; }
+    }
+}
