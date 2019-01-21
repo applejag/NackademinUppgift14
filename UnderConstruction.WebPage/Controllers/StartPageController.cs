9dc66cbc23d277b1e9926768742068cb6caa30ac
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-21 16:58:00 +0100

    Added startpage controller and view

---
+++ UnderConstruction.WebPage/Controllers/StartPageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Web.Mvc;
+using UnderConstruction.WebPage.Models.Pages;
+
+namespace UnderConstruction.WebPage.Controllers
+{
+    public class StartPageController : PageControllerBase<StartPage>
+    {
+        public StartPageController(IContentLoader loader) : base(loader)
+        {
+        }
+
+        public ActionResult Index(StartPage currentPage)
+        {
+            return View(CreatePageViewModel(currentPage));
+        }
+    }
+}
