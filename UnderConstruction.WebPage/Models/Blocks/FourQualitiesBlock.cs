51e374be77ebce98f7e6a17c04b7f83bd669443c
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-24 15:34:41 +0100

    Added four-qualities block type

---
+++ UnderConstruction.WebPage/Models/Blocks/FourQualitiesBlock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using EPiServer.Web;
+using UnderConstruction.WebPage.Models.Media;
+
+namespace UnderConstruction.WebPage.Models.Blocks
+{
+    [ContentType(DisplayName = "Four qualities",
+        GUID = "133a378e-3846-4913-afab-0e98645e2e82",
+        Description = "Use this to display four qualities (with images) and short descriptions about each quality.")]
+    public class FourQualitiesBlock : BlockData
+    {
+        public const string QualitiesTab = "4 Qualities";
+
+        [Required]
+        [Display(Name = "Heading",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [Display(Name = "Content text",
+            GroupName = SystemTabNames.Content,
+            Order = 30)]
+        public virtual XhtmlString Description { get; set; }
+
+        [Display(Name = "Quality #1",
+            GroupName = QualitiesTab,
+            Order = 10)]
+        public virtual SingleQuality Quality1 { get; set; }
+
+        [Display(Name = "Quality #2",
+            GroupName = QualitiesTab,
+            Order = 20)]
+        public virtual SingleQuality Quality2 { get; set; }
+
+        [Display(Name = "Quality #3",
+            GroupName = QualitiesTab,
+            Order = 30)]
+        public virtual SingleQuality Quality3 { get; set; }
+
+        [Display(Name = "Quality #4",
+            GroupName = QualitiesTab,
+            Order = 40)]
+        public virtual SingleQuality Quality4 { get; set; }
+    }
+}
---
+++ UnderConstruction.WebPage/Models/Blocks/SingleQuality.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Web;
+
+namespace UnderConstruction.WebPage.Models.Blocks
+{
+    [ContentType(DisplayName = "Single quality", 
+        GUID = "ebe8da93-1132-43a7-848a-eba85e34fd2f",
+        Description = "",
+        AvailableInEditMode = false)]
+    public class SingleQuality : BlockData
+    {
+        [Required]
+        [Display(Name = "Quality title",
+            Order = 10)]
+        public virtual string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Quality description",
+            Order = 20)]
+        [UIHint(UIHint.Textarea)]
+        public virtual string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Quality icon name (font-awesome v3, without \"fa-\")",
+            Order = 30)]
+        public virtual string Icon { get; set; }
+    }
+}
