fa9c91acce8d1182da276dc62765f2b5951f9c0b
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-24 21:52:32 +0100

    Forgot to add teaser controller

---
+++ UnderConstruction.WebPage/Controllers/TeaserBlockController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Web.Mvc;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using UnderConstruction.WebPage.Models.Blocks;
+using UnderConstruction.WebPage.Models.ViewModels;
+
+namespace UnderConstruction.WebPage.Controllers
+{
+    [TemplateDescriptor(Tags = new[] {SiteTags.Full},
+        AvailableWithoutTag = true)]
+    public class TeaserBlockController : BlockController<TeaserBlock>
+    {
+        public override ActionResult Index(TeaserBlock currentBlock)
+        {
+            var model = new TeaserBlockViewModel
+            {
+                CurrentBlock = currentBlock,
+                TodayVisitorCount = new Random().Next(900, 10900)
+            };
+
+            return PartialView(model);
+        }
+    }
+
+    [TemplateDescriptor(Tags = new[] {SiteTags.Wide},
+        AvailableWithoutTag = false)]
+    public class TeaserBlockWideController : BlockController<TeaserBlock>
+    {
+        public override ActionResult Index(TeaserBlock currentBlock)
+        {
+            var model = new TeaserBlockViewModel
+            {
+                CurrentBlock = currentBlock,
+                TodayVisitorCount = new Random().Next(900, 10900)
+            };
+
+            return PartialView(model);
+        }
+    }
+
+    [TemplateDescriptor(Tags = new[] {SiteTags.Narrow},
+        AvailableWithoutTag = false)]
+    public class TeaserBlockNarrowController : BlockController<TeaserBlock>
+    {
+        public override ActionResult Index(TeaserBlock currentBlock)
+        {
+            var model = new TeaserBlockViewModel
+            {
+                CurrentBlock = currentBlock,
+                TodayVisitorCount = new Random().Next(900, 10900)
+            };
+
+            return PartialView(model);
+        }
+    }
+
+}
---
+++ UnderConstruction.WebPage/SiteTags.cs
@@ -0,0 +1,9 @@
+﻿namespace UnderConstruction.WebPage
+{
+    public static class SiteTags
+    {
+        public const string Full = "Full";
+        public const string Wide = "Wide";
+        public const string Narrow = "Narrow";
+    }
+}
