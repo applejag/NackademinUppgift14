367f2d3206345bddf8de729b9037f3273faf3029
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-24 21:45:55 +0100

    Added content area block type

---
+++ UnderConstruction.WebPage/Models/Blocks/ContentAreaBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Web;
+
+namespace UnderConstruction.WebPage.Models.Blocks
+{
+    [ContentType(DisplayName = "Content area",
+        GUID = "ad4abd7b-518a-4767-991f-54e0688d2fe2",
+        Description = "Use this for a collection of contents in it's content area.")]
+    public class ContentAreaBlock : BlockData
+    {
+        [Display(Name = "Heading",
+            GroupName = SystemTabNames.PageHeader,
+            Order = 10)]
+        [CultureSpecific]
+        public virtual string Heading { get; set; }
+
+        [Display(Name = "Content area",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual ContentArea ContentArea { get; set; }
+    }
+}
