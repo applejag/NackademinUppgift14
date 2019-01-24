05a6005233e54dd2167142b0749332b08ad8cbf6
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-24 22:47:57 +0100

    Added preview page, controller and view

---
+++ UnderConstruction.WebPage/Controllers/PreviewPageController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Framework.Web;
+using EPiServer.Web;
+using EPiServer.Web.Mvc;
+using UnderConstruction.WebPage.Models.Pages;
+using UnderConstruction.WebPage.Models.ViewModels;
+
+namespace UnderConstruction.WebPage.Controllers
+{
+    [TemplateDescriptor(Inherited = true,
+        TemplateTypeCategory = TemplateTypeCategories.MvcController,
+        Tags = new[] { RenderingTags.Preview },
+        AvailableWithoutTag = false)]
+    public class PreviewPageController : ActionControllerBase, IRenderTemplate<BlockData>
+    {
+        private readonly IContentLoader _loader;
+
+        public PreviewPageController(IContentLoader loader)
+        {
+            _loader = loader;
+        }
+
+        public ActionResult Index(IContent currentContent)
+        {
+            var startPage = _loader.Get<StartPage>(ContentReference.StartPage);
+            var model = new PreviewPageViewModel(startPage, currentContent);
+            return View(model);
+        }
+    }
+}
---
+++ UnderConstruction.WebPage/Models/ViewModels/PreviewPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer.Core;
+using UnderConstruction.WebPage.Models.Pages;
+
+namespace UnderConstruction.WebPage.Models.ViewModels
+{
+    public class PreviewPageViewModel : PageViewModel<SitePageData>
+    {
+        public ContentArea PreviewArea { get; set; }
+
+        public PreviewPageViewModel(SitePageData currentPage, IContent contentToPreview)
+            : base(currentPage)
+        {
+            PreviewArea = new ContentArea();
+            PreviewArea.Items.Add(new ContentAreaItem { ContentLink = contentToPreview.ContentLink });
+        }
+    }
+}
