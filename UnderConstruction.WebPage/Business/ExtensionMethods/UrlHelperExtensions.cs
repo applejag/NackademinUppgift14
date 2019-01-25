2b71b131c153b52d1d50a15806309ce25686ea39
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-25 09:51:12 +0100

    Added login button in footer

---
+++ UnderConstruction.WebPage/Business/ExtensionMethods/UrlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
+
+namespace UnderConstruction.WebPage.Business.ExtensionMethods
+{
+    public static class UrlHelperExtensions
+    {
+        public static string Action(this UrlHelper url, ContentReference contentLink, string action = "index")
+        {
+            var resolver = ServiceLocator.Current.GetInstance<UrlResolver>();
+
+            return resolver.GetUrl(contentLink, null, new VirtualPathArguments
+            {
+                Action = action
+            });
+        }
+    }
+}
